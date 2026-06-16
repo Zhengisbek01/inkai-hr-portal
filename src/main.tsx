2d1d41ea4a77948492142d6cfff05e306abcc26f
Zhengisbek01 <jasa4b@gmail.com>
2026-06-16 06:47:32 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 // @ts-ignore
 import App from './hr-app.jsx'
 
-const root = document.getElementById('root')!
-
-ReactDOM.createRoot(root).render(
-  React.createElement(App)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
